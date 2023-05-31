8f6e62134eb1eed53731e096c46e671206b05f34
Zevs <xXx3evsxXx@gmail.com>
2023-05-31 16:10:53 +0300

    Implemented simple page for CRUD Goods in the shop

--- goods_page/models.py
+++ goods_page/models.py
@@ -1,5 +1,44 @@
 from django.db import models
+from enum import Enum
+
+
+class SizeEnum(Enum):
+    S_SMALL = "S"
+    M_MEDIUM = "M"
+    L_LARGE = "L"
+    XL_SUPER_LARGE = "XL"
+
+
+class SexEnum(Enum):
+    FEMALE = "Female"
+    MALE = "Male"
 
 
 class Product(models.Model):
-    
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    size = models.CharField(
+        max_length=2, choices=[(choice.value, choice.name) for choice in SizeEnum]
+    )
+    sex = models.CharField(
+        max_length=6, choices=[(choice.value, choice.name) for choice in SexEnum]
+    )
+    price = models.FloatField()
+    on_discount = models.BooleanField(default=False)
+    discount = models.PositiveIntegerField(default=17)
+    discount_start_date = models.DateTimeField(null=True)
+    discount_start_end = models.DateTimeField(null=True)
+
+    class Meta:
+        ordering = ["-on_discount", "name"]
+        unique_together = ("name", "size")
+
+    @property
+    def price_with_discount(self) -> float:
+        if self.on_discount is True:
+            return round(self.price * (1 - self.discount / 100), 2)
+
+    def __str__(self) -> str:
+        if self.on_discount is True:
+            return f"Name: {self.name}, Price: {self.price}, PWD: {self.price_with_discount}, Size: {self.size}"
+        return f"Name: {self.name}, Price: {self.price}, Size: {self.size}"
